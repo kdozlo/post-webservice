ef4352ca709876debfcbed57d44b4d60e9460992
kdozlo <eodud5730@naver.com>
2023-04-17 02:03:48 +0900

    초기 settings

---
+++ src/main/java/webstudy/postwebservice/PostWebServiceApplication.java
@@ -0,0 +1,13 @@
+package webstudy.postwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webstudy/postwebservice/PostWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package webstudy.postwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
