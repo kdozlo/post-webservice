f4c7c7a2507480a4e766097e3df4a0b0aee8ae6e
kdozlo <eodud5730@naver.com>
2023-04-17 19:24:06 +0900

    BaseTimeEntity setting

--- src/main/java/webstudy/postwebservice/domain/posts/Posts.java
+++ src/main/java/webstudy/postwebservice/domain/posts/Posts.java
@@ -3,13 +3,14 @@ package webstudy.postwebservice.domain.posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import webstudy.postwebservice.domain.BaseTimeEntity;
 
 import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
