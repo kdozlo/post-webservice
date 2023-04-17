61589518a6c5c002ed563de43842ba6623edb745
kdozlo <eodud5730@naver.com>
2023-04-17 14:03:14 +0900

    PostsService 추가

---
+++ src/main/java/webstudy/postwebservice/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package webstudy.postwebservice.service.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import webstudy.postwebservice.domain.posts.PostsRepository;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
