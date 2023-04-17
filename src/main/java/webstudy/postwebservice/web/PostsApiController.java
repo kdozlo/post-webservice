c70ce247ee2333556d602185f77e2fac6b9816fb
kdozlo <eodud5730@naver.com>
2023-04-17 16:15:16 +0900

    PostsController - update api, findById api 추가

--- src/main/java/webstudy/postwebservice/web/PostsApiController.java
+++ src/main/java/webstudy/postwebservice/web/PostsApiController.java
@@ -1,9 +1,7 @@
 package webstudy.postwebservice.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import webstudy.postwebservice.service.posts.PostsService;
 import webstudy.postwebservice.web.dto.PostsSaveRequestDto;
 
@@ -16,5 +14,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
