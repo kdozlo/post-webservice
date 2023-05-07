a2a9de1c386ca8f5f4857779b65a862abe5f6de1
kdozlo <eodud5730@naver.com>
2023-05-08 08:25:11 +0900

    indexController - index 구현

--- src/main/java/webstudy/postwebservice/web/IndexController.java
+++ src/main/java/webstudy/postwebservice/web/IndexController.java
@@ -1,13 +1,21 @@
 package webstudy.postwebservice.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import webstudy.postwebservice.service.posts.PostsService;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
