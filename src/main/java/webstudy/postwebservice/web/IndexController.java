4db3a70f1ec104a3b2171d04bbcb3de809e174a2
kdozlo <eodud5730@naver.com>
2023-05-09 16:33:38 +0900

    indexController - update 구현

--- src/main/java/webstudy/postwebservice/web/IndexController.java
+++ src/main/java/webstudy/postwebservice/web/IndexController.java
@@ -4,7 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import webstudy.postwebservice.service.posts.PostsService;
+import webstudy.postwebservice.web.dto.PostsUpdateRequestDto;
 
 @RequiredArgsConstructor
 @Controller
@@ -22,5 +26,11 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody
+                       PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
     }
 }
