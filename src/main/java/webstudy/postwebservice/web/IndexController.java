896e3d06f7e7ed9bec2184702d79248a9b7901b6
kdozlo <eodud5730@naver.com>
2023-05-10 11:57:56 +0900

    로그인 구현으로 IndexController 수정

--- src/main/java/webstudy/postwebservice/web/IndexController.java
+++ src/main/java/webstudy/postwebservice/web/IndexController.java
@@ -7,20 +7,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import webstudy.postwebservice.config.auth.dto.SessionUser;
 import webstudy.postwebservice.service.posts.PostsService;
 import webstudy.postwebservice.web.dto.PostsResponseDto;
 import webstudy.postwebservice.web.dto.PostsUpdateRequestDto;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
 
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession
+                .getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
