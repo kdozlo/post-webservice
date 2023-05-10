b7df507b415b05d9512c773269e0c05b02ce5812
kdozlo <eodud5730@naver.com>
2023-05-10 15:39:39 +0900

    WebConfig 생성

---
+++ src/main/java/webstudy/postwebservice/config/WebConfig.java
@@ -0,0 +1,23 @@
+package webstudy.postwebservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import webstudy.postwebservice.config.auth.LoginUser;
+import webstudy.postwebservice.config.auth.LoginUserArgumentResolver;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+
+
+}
