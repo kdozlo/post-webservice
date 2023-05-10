8934e2ccc22cc366dd2972bb365f21ed48463c35
kdozlo <eodud5730@naver.com>
2023-05-10 10:47:18 +0900

    SecurityConfig 추가 - WebSecurityConfigurerAdapter Depreated로 인해 SecurityFilterChain을 빈 등록

---
+++ src/main/java/webstudy/postwebservice/config/auth/SecurityConfig.java
@@ -0,0 +1,39 @@
+package webstudy.postwebservice.config.auth;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import webstudy.postwebservice.domain.user.Role;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                .logout()
+                    .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                    .userInfoEndpoint()
+                        .userService(customOAuth2UserService);
+
+        return http.build();
+    }
+}
